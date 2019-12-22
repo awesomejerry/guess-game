e1889438b37bbcd915553a634d314ad68734acd5
JerryShen <jerry82422@gmail.com>
2019-12-22 23:53:58 +0800

    Update code

--- src/guess_number/core.clj
+++ src/guess_number/core.clj
@@ -1,7 +1,26 @@
 (ns guess-number.core
   (:gen-class))
 
+(defn parse-int [x] (Integer/parseInt x))
+(defn enter-max [] (parse-int (do (print "Please enter maximum number: ")
+                                  (flush)
+                                  (read-line))))
+(defn enter-answer [] (parse-int (do (print "Please enter your answer: ")
+                                     (flush)
+                                     (read-line))))
+(defn verify-answer [answer max equal]
+  (let [input (enter-answer)]
+    (if (= answer input)
+      (equal)
+      (do
+        (if (> answer input)
+          (println (str "It's between " input " and " max))
+          (println (str "It's between 0 and " input)))
+        (verify-answer answer max equal)))))
+
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [max (enter-max)
+        answer (rand-int max)]
+    (verify-answer answer max #(println "You hit the answer!"))))
